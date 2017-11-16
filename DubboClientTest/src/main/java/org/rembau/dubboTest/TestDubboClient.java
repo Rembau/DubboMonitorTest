571986fbc54eb66921287dfbc551a7f817d582c8
rembau <504229406@qq.com>
2017-11-16 14:49:19 +0800

    提交测试

--- DubboClientTest/src/main/java/org/rembau/dubboTest/TestDubboClient.java
+++ DubboClientTest/src/main/java/org/rembau/dubboTest/TestDubboClient.java
@@ -1,9 +1,13 @@
 package org.rembau.dubboTest;
 
-import com.unilife.common.dto.push.DeviceFilterParam;
+import com.unilife.common.dto.push.FilterParam;
 import com.unilife.common.dto.push.PushDataContext;
 import com.unilife.common.dto.push.PushDataDto;
+import com.unilife.common.dto.push.PushMessageDto;
 import com.unilife.common.service.push.DataPushService;
+import com.unilife.media.dto.message.type.MessageGroupInfoDto;
+import com.unilife.media.dto.message.type.MessageTypeInfoDto;
+import com.unilife.utils.Constant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -28,29 +34,41 @@ public class TestDubboClient {
     public void test() {
         logger.info("测试推送==================================");
         PushDataDto pushDataDto = new PushDataDto();
-        pushDataDto.setImmediate(true);
+
+        //推送时间
+        pushDataDto.setImmediate(false);
+        pushDataDto.setAdvance(true);
+        pushDataDto.setAdvanceTime(2 * 60 * 60 * 1000);
 
         PushDataContext pushDataContext = new PushDataContext();
-        pushDataContext.setDataType("000000");
-        pushDataContext.setEffectiveTime(new Date());
-        pushDataContext.setMessageType(1);
-        pushDataContext.setData(
-                "{\n" +
-                        "  \"messageTitle\":\"优惠券到账提醒\",\n" +
-                        "  \"messageContent\":\"balabala\"\n" +
-                        "}");
+        pushDataContext.setDataType(Constant.DataPush.DATA_TYPE_MESSAGE);
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2017-05-09 15:45:20");
+            pushDataContext.setEffectiveTime(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        pushDataContext.setMessageType(Constant.DataPush.MESSAGE_TYPE_DATA_CHANGE);
+
+        PushMessageDto pushMessageDto = new PushMessageDto();
+        pushMessageDto.setMessageGroup(MessageGroupInfoDto.SYSTEM.getId());
+        pushMessageDto.setMessageType(MessageTypeInfoDto.NEW_COUPON.getId());
+        pushMessageDto.setMessageTitle(MessageTypeInfoDto.NEW_COUPON.getName());
+
+        pushDataContext.setData(pushMessageDto);
         pushDataDto.setData(pushDataContext);
 
-        DeviceFilterParam filterParam = new DeviceFilterParam();
+        FilterParam filterParam = new FilterParam();
         filterParam.setClientType(1);
-        //filterParam.setDeviceIdList(new HashSet<String>());
-        //filterParam.getDeviceIdList().add("7864e6176fed");
+        filterParam.setDeviceIdList(new HashSet<String>());
+        filterParam.getDeviceIdList().add("testDeviceCode");
 
         /*filterParam.setCityList(new HashSet<String>());
         filterParam.getCityList().add("上海市");*/
 
-        filterParam.setDeviceModelList(new HashSet<String>());
-        filterParam.getDeviceModelList().add("kqd428lga");
+        /*filterParam.setDeviceModelList(new HashSet<String>());
+        filterParam.getDeviceModelList().add("kqd428lga");*/
+
         pushDataDto.setFilterParam(filterParam);
 
         dataPushService.pushMessage(pushDataDto);
